97a2007afa14c424e69561eba7b6d0bce8fde443
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-08-16 17:37:00 -0700

    :construction: (Sensor) WIP acceleromteter

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #define HELTEC_POWER_BUTTON // must be before "#include <heltec_unofficial.h>"
 
 #include <Arduino.h>
+#include <SparkFun_BMI270_Arduino_Library.h>
+#include <Wire.h>
 #include <heltec_unofficial.h>
 // https://github.com/ropg/heltec_esp32_lora_v3
 
 // Uncomment this line to use the board as a transmitter
 // Comment it out to use as a receiver
-// #define TRANSMITTER_MODE
+#define TRANSMITTER_MODE
 
 // LoRa parameters
 #define FREQUENCY 915.0
@@ -15,13 +17,19 @@
 #define CODING_RATE 5
 #define TX_POWER 10
 
+BMI270 imu;
+
+// I2C address selection
+// uint8_t i2cAddress = BMI2_I2C_PRIM_ADDR; // 0x68
+uint8_t i2cAddress = BMI2_I2C_SEC_ADDR; // 0x69
+
 unsigned long lastDisplayUpdate = 0;
 const unsigned long displayUpdateInterval =
     30000; // Update display every 30 seconds
 
 void transmitterLoop() {
   static unsigned long lastTransmitTime = 0;
-  const unsigned long transmitInterval = 5000; // 5 seconds
+  const unsigned long transmitInterval = 500; // 500 ms
 
   if (millis() - lastTransmitTime > transmitInterval) {
     String packet = "Packet #" + String(millis() / 1000);
@@ -44,6 +52,16 @@ void transmitterLoop() {
       Serial.println(state);
       display.drawString(0, 32, "Send failed, code " + String(state));
     }
+
+    // Read accelerometer data
+    imu.getSensorData();
+
+    Serial.print("Accel X: ");
+    Serial.print(imu.data.accelX);
+    Serial.print(" Y: ");
+    Serial.print(imu.data.accelY);
+    Serial.print(" Z: ");
+    Serial.println(imu.data.accelZ);
 
     display.display();
     lastTransmitTime = millis();
@@ -105,6 +123,35 @@ void receiverLoop() {
 void setup() {
   heltec_setup();
   Serial.println("Heltec board initialized");
+
+  // Wire.begin(4, 5); // SDA = GPIO4, SCL = GPIO5
+  // Wire.begin(); // SDA = GPIO4, SCL = GPIO5
+  //    while(imu.beginI2C(i2cAddress) != BMI2_OK)
+
+  // // if (imu.beginI2C() != 0) {
+  //   Serial.println("Error: BMI270 not detected");
+  //   Serial.println(imu.beginI2C());
+  //   while (1)
+  //     ;
+  // }
+  //
+  // pinMode(RST_OLED, OUTPUT);
+  // digitalWrite(RST_OLED, LOW);
+  // delay(50);
+  // digitalWrite(RST_OLED, HIGH);
+  // Wire.begin(SDA_OLED, SCL_OLED); // Scan OLED's I2C address via I2C0
+
+  Wire.begin();
+
+  while (imu.beginI2C(i2cAddress) != BMI2_OK) {
+    // Not connected, inform user
+    Serial.println(
+        "Error: BMI270 not connected, check wiring and I2C address!");
+
+    // Wait a bit to see if connection is established
+    delay(1000);
+  }
+  Serial.println("BMI270 connected!");
 
   // Initialize LoRa
   Serial.print("Initializing LoRa: ");
